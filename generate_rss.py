d4ed1cb476a859b475866f4d1951f8820677892b
つぶ <info@tbshiki.com>
2024-04-01 12:37:16 +0900

    update chage library

--- generate_rss.py
+++ generate_rss.py
@@ -1,50 +1,58 @@
 import feedparser
-import PyRSS2Gen
-import datetime
+from feedgen.feed import FeedGenerator
+from datetime import datetime
+import pytz
 
-# 読み込みたいRSSフィードのURL
-feed_urls = [
-    "https://wptavern.com/feed",
-    "https://kinsta.com/blog/feed/",
-    "https://www.wpbeginner.com/feed/",
-    "https://gutenbergtimes.com/feed/",
-]
+# 日本時間のタイムゾーンを設定
+JST = pytz.timezone("Asia/Tokyo")
 
-# 全てのフィードエントリを保持するリスト
-all_entries = []
 
-# 各フィードを読み込み、エントリをリストに追加
-for url in feed_urls:
-    feed = feedparser.parse(url)
-    all_entries.extend(feed.entries)
+def generate_rss_feed():
 
-# エントリを公開日でソート
-all_entries.sort(key=lambda entry: entry.published_parsed, reverse=True)
+    # 読み込みたいRSSフィードのURL
+    feed_urls = [
+        "https://wptavern.com/feed",
+        "https://kinsta.com/blog/feed/",
+        "https://www.wpbeginner.com/feed/",
+        "https://gutenbergtimes.com/feed/",
+    ]
 
-# 新しいRSSフィードの項目を生成
-rss_items = []
-for entry in all_entries:
-    rss_items.append(
-        PyRSS2Gen.RSSItem(
-            title=entry.title,
-            link=entry.link,
-            description=entry.description,
-            pubDate=datetime.datetime(*entry.published_parsed[:6]),
-        )
+    # 全てのフィードエントリを保持するリスト
+    all_entries = []
+
+    # 各フィードを読み込み、エントリをリストに追加
+    for url in feed_urls:
+        feed = feedparser.parse(url)
+        all_entries.extend(feed.entries)
+
+    # エントリを公開日でソート
+    # 'published_parsed'がない場合は現在時刻を使用する
+    all_entries.sort(
+        key=lambda entry: entry.get(
+            "published_parsed", datetime.now(tz=JST).timetuple()
+        ),
+        reverse=True,
     )
 
-# 新しいRSSフィードを生成
-rss = PyRSS2Gen.RSS2(
-    title="WP do",
-    link="https://tbshiki.github.io/wp-do-rss/",
-    description="WP do RSS feed",
-    lastBuildDate=datetime.datetime.now(),
-    items=rss_items,
-)
+    # 新しいRSSフィードを初期化
+    fg = FeedGenerator()
+    fg.title("WP do")
+    fg.link(href="https://tbshiki.github.io/wp-do-rss/", rel="alternate")
+    fg.description("WP do RSS feed")
+    fg.lastBuildDate(datetime.now(JST))
 
-# ファイルに書き出す前にRSSフィードをUTF-8のXML文字列として生成
-rss_xml = rss.to_xml(encoding="utf-8")
+    # 新しいRSSフィードの項目を追加
+    for entry in all_entries:
+        fe = fg.add_entry()
+        fe.title(entry.title)
+        fe.link(href=entry.link)
+        fe.description(entry.description)
+        if hasattr(entry, "published"):
+            fe.published(entry.published)
 
-# RSSフィードをファイルに書き出す
-with open("feed/index.xml", "w", encoding="utf-8") as f:
-    f.write(rss_xml)
+    # RSSフィードをファイルに書き出す
+    fg.rss_file("feed/index.xml", pretty=True, encoding="utf-8")
+
+
+if __name__ == "__main__":
+    generate_rss_feed()
